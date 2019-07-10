b3664fc3a868e5bce54eb4efbc65e67ac4785643
learn <erinstalzer@gmail.com>
2019-07-10 13:16:35 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,16 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params["artist_id"]
+      @artist = Artist.find_by(id: params["artist_id"])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found."
+      else
+        @song = @artist.songs.build
+      end
+    else 
+      @song = Song.new
+    end
   end
 
   def create
@@ -39,7 +48,33 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    
+    if params["artist_id"]
+      @artist = Artist.find_by(id: params["artist_id"])
+      if @artist
+        @song = @artist.songs.find_by(id: params["id"])
+        if @song.nil?
+          redirect_to artist_songs_path(@artist), alert: "Song not found."
+        end
+      else
+        redirect_to artists_path
+      end
+    # if params["id"]
+    #   @song = Song.find_by(id: params["id"])
+    #   if @song.nil?
+    #     redirect_to artist_songs_path(@artist), alert: "Song not found."
+    #   else
+    #     if @artist 
+    #       #binding.pry
+    #       #@song = @artist.songs.find_by(id: params["id"])
+    #       redirect_to edit_song_path(@song)
+    #     else
+    #       redirect_to artists_path
+    #     end
+    #   end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -64,7 +99,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_id)
   end
 end
 
